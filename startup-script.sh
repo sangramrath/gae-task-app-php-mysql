27809f2d41df5869f018b1df2805acdebe126412
sangramrath <sangram.rath@gmail.com>
2021-09-04 17:50:23 +0530

    startup script v1
    Startup Script to configure Cloud VM

---
+++ startup-script.sh
@@ -0,0 +1,9 @@
+apt update
+apt install git curl apache2 apache2-utils
+apt install php7.3 libapache2-mod-php7.3 php7.3-mysql
+a2enmod php7.3
+a2enmod rewrite
+systemctl restart apache2
+
+git clone https://github.com/sangramrath/task-app-php-mysql.git /var/www/html/
+chown www-data:www-data /var/www/html/
