7317cf1af7926d904061b762344cad13673d9622
sangramrath <sangram.rath@gmail.com>
2021-09-05 08:43:13 +0530

    startup script correction

--- taskapp-startup-script.sh
+++ taskapp-startup-script.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-apt update
-apt install git curl apache2 apache2-utils
-apt install php7.3 libapache2-mod-php7.3 php7.3-mysql
-a2enmod php7.3
-a2enmod rewrite
-systemctl restart apache2
+set -e
+export HOME=/root
 
+apt update
+apt-get install -y git curl apache2 apache2-utils
+apt-get install -y php php-mysql libapache2-mod-php
+
+rm -rf /var/www/html/
 git clone https://github.com/sangramrath/task-app-php-mysql.git /var/www/html/
-chown www-data:www-data /var/www/html/
+
+chown -R www-data:www-data /var/www/html/
+
+a2enmod rewrite
+
+systemctl restart apache2
