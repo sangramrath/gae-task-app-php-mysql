09c001ea26a104015214e69cfd5603c48d227014
sangramrath <sangram.rath@gmail.com>
2021-09-13 23:05:52 +0530

    Update db.php to make it work with GAE

--- db.php
+++ db.php
@@ -1,11 +1,6 @@
 <?php
 session_start();
 
-$conn = mysqli_connect(
-  'localhost',
-  'root',
-  'Pa55w0rd',
-  'task'
-) or die(mysqli_erro($mysqli));
+$conn = mysqli_connect(null, 'root' ,'Pa55w0rd', 'task', null , '/cloudsql/[SQL_INSTANCE_CONNECTION_NAME]');
 
 ?>
